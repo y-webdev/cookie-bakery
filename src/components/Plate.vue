4826dde47ec952952a5f0210b9f0d85aa9e9ce94
Yanislav Pavlov <yanislav.pavlov@gmail.com>
2021-03-16 18:16:40 +0100

    Add plate

--- src/components/Plate.vue
+++ src/components/Plate.vue
@@ -1,12 +1,18 @@
 <template>
+    <div class="w-50 text-uppercase text-center py-3 h3 font-weight-bold">Cookie Bakery</div>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
 <style scoped>
-
+div {
+    position:absolute;
+    top:-2.25rem;
+    left: 50%;
+    transform: translateX(-50%);
+    background: mediumseagreen;
+    border: .2rem solid darkolivegreen;
+}
 </style>
