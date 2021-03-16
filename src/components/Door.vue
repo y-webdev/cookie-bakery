ae68d0e8936ba756c90d0bdfa8719fff21bbefeb
Yanislav Pavlov <yanislav.pavlov@gmail.com>
2021-03-16 18:18:12 +0100

    Add door

--- src/components/Door.vue
+++ src/components/Door.vue
@@ -1,11 +1,26 @@
 <template>
+    <button class="btn h-75 w-100 position-absolute"></button>
 </template>
 
 <script>
-export default {
-}
+export default {}
 </script>
 
 <style scoped>
-
+button {
+    bottom:0;
+    left:0;
+    background: lightsalmon;
+}
+button:after {
+    content:"";
+    position:absolute;
+    top:50%;
+    left:2rem;
+    transform: translateX(-50%);
+    width: 3.25rem;
+    height: .75rem;
+    background: saddlebrown;
+    border-radius: .25rem;
+}
 </style>
